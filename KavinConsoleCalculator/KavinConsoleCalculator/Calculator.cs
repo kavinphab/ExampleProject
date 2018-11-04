5a06a1c3119b7afc01cc35dde91b3a2129af7b53
joeyzli <joeyzhuoyili@gmail.com>
2018-11-04 12:51:45 -0800

    calculator initial upload

---
+++ KavinConsoleCalculator/KavinConsoleCalculator/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KavinConsoleCalculator
+{
+    class Calculator
+    {
+
+        // create a function to add, subtract, divide, multiply, mod, square
+        public static int Add (int num1, int num2)
+        {
+            int sum = 0;
+            sum = num1 + num2;
+            return sum;
+        }
+            
+
+    }
+}
---
+++ KavinConsoleCalculator/KavinConsoleCalculator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KavinConsoleCalculator
+{
+    class Program
+    {
+
+        static int x;
+
+
+        static void Main(string[] args)
+        {
+            //Calculator calculator = new Calculator();
+
+
+            //calculator.Add(5, 6);
+
+
+
+            Console.WriteLine(  Calculator.Add(5, 6));
+
+
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
